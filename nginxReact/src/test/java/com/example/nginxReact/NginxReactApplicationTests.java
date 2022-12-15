0de5d311c885a0d972d62b816676851fe38121bf
nhj1987 <nhj1987@cj.net>
2022-12-15 10:24:31 +0900

    [초기커밋]

---
+++ nginxReact/src/test/java/com/example/nginxReact/NginxReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginxReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
